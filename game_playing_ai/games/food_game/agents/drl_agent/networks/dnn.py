3d5d004ec1523358677dab8effc3f535bcf8cdeb
abbymark <abbymark@naver.com>
2024-01-31 21:27:06 +0900

    [UPDATE] use bigger model

--- game_playing_ai/games/food_game/agents/drl_agent/networks/dnn.py
+++ game_playing_ai/games/food_game/agents/drl_agent/networks/dnn.py
@@ -6,17 +6,18 @@ class DNN(torch.nn.Module):
         self.input_dim = input_dim
         self.output_dim = output_dim
         self.fc1 = torch.nn.Linear(input_dim, 128)
-        self.fc2 = torch.nn.Linear(128, 128)
-        self.fc3 = torch.nn.Linear(128, output_dim)
+        self.fc2 = torch.nn.Linear(128, 256)
+        self.fc3 = torch.nn.Linear(256, 64)
+        self.fc4 = torch.nn.Linear(64, output_dim)
         self.relu = torch.nn.ReLU()
-        self.softmax = torch.nn.Softmax(dim = 1)
     
     def forward(self, x):
         x = self.fc1(x)
         x = self.relu(x)
         x = self.fc2(x)
-        x = self.relu(x) 
+        x = self.relu(x)
         x = self.fc3(x)
-        x = self.softmax(x)
+        x = self.relu(x)
+        x = self.fc4(x)
         return x
     
