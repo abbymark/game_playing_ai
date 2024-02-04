f194ceea16cf732cd35177de8eb233365a2d8216
abbymark <abbymark@naver.com>
2024-02-04 21:24:10 +0900

    [ADD] test cases

---
+++ test/test_game_playing_ai/test_games/test_food_game/test_food_game.py
@@ -0,0 +1,82 @@
+from game_playing_ai.games.food_game.food_game import FoodGame, GridFoodGame, train_drl_agent
+
+class TestFoodGame:
+    def test__init__game__human_render_mode(self):
+        game = FoodGame(rows=30, cols=40, n_food=10, render_mode='human', is_training=False, solo_training=False)
+        assert game.render_mode == 'human'
+        assert hasattr(game, 'run_speed')
+        assert hasattr(game, 'clock')
+        assert hasattr(game, 'canvas')
+        assert hasattr(game, 'environment')
+
+        assert hasattr(game, 'map')
+        assert hasattr(game, 'playable_agent')
+        assert hasattr(game, 'preprogrammed_agent')
+        assert hasattr(game, 'drl_agent')
+        assert hasattr(game, 'drl_agent_sprite')
+        assert hasattr(game, 'foods')
+
+        has_playable_agent = False
+        has_preprogrammed_agent = False
+        has_drl_agent_sprite = False
+        food_count = 0
+        for row in game.map:
+            for cell in row:
+                if cell == 2:
+                    food_count += 1
+                elif cell == 3:
+                    has_playable_agent = True
+                elif cell == 4:
+                    has_preprogrammed_agent = True
+                elif cell == 5:
+                    has_drl_agent_sprite = True
+        
+        assert food_count == 10
+        assert has_playable_agent
+        assert has_preprogrammed_agent
+        assert has_drl_agent_sprite
+    
+    def test__init__game__rgb_array_render_mode(self):
+        game = FoodGame(rows=30, cols=40, n_food=10, render_mode='rgb_array', is_training=False, solo_training=False)
+        assert game.render_mode == 'rgb_array'
+        assert not hasattr(game, 'run_speed')
+        assert not hasattr(game, 'clock')
+        assert not hasattr(game, 'canvas')
+        assert not hasattr(game, 'environment')
+        
+        assert hasattr(game, 'map')
+        assert hasattr(game, 'playable_agent')
+        assert hasattr(game, 'preprogrammed_agent')
+        assert hasattr(game, 'drl_agent')
+        assert hasattr(game, 'drl_agent_sprite')
+        assert hasattr(game, 'foods')
+
+        has_playable_agent = False
+        has_preprogrammed_agent = False
+        has_drl_agent_sprite = False
+        food_count = 0
+        for row in game.map:
+            for cell in row:
+                if cell == 2:
+                    food_count += 1
+                elif cell == 3:
+                    has_playable_agent = True
+                elif cell == 4:
+                    has_preprogrammed_agent = True
+                elif cell == 5:
+                    has_drl_agent_sprite = True
+        
+        assert food_count == 10
+        assert has_playable_agent
+        assert has_preprogrammed_agent
+        assert has_drl_agent_sprite
+
+
+
+class TestGridFoodGame:
+    @classmethod
+    def setup_class(cls):
+        cls.game = GridFoodGame(rows=30, cols=40, n_food=10, render_mode='human', is_training=False, solo_training=False)
+
+    def test__init__game__human_render_mode(self):
+        ...
